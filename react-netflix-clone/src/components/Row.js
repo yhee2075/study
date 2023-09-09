63a32058cc42b3cfcf88dab1706171b216854833
yhee <yhee2075@naver.com>
2023-09-09 20:31:24 +0900

    섹션5.슬라이드_기능_추가하기

--- react-netflix-clone/src/components/Row.js
+++ react-netflix-clone/src/components/Row.js
@@ -20,23 +20,34 @@ export default function Row({title, id, fetchUrl, isLargeRow}) {
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{'<'}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {'<'}
+          </span>
         </div>
-      </div>
-
-      <div id={id} className="row__posters">
-        {movies.map(movie => (
-          <img
-            key={movie.id}
-            className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
-            src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
-            alt={movie.name}
-          />
-        ))}
-      </div>
-      <div className="slider">
+        <div id={id} className="row__posters">
+          {movies.map(movie => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && 'row__posterLarge'}`}
+              src={`https://image.tmdb.org/t/p/original/${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
         <div className="slider__arrow-right">
-          <span className="arrow">{'>'}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {'>'}
+          </span>
         </div>
       </div>
     </section>
