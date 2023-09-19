7108cab2d44060eca9d8b67b9cf7c050beaf8869
yhee <yhee2075@naver.com>
2023-09-19 21:24:31 +0900

    섹션5.env_이용하여_API_KEY_환경변수로_숨기기

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: 'dc6c8d24cda583b4ccde487443bd705f',
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: 'ko-KR',
   },
 });
